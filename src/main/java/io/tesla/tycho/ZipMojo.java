53d0bb8e76460130d36b2069399a5de9c935d6d6
Jason van Zyl <jason@sonatype.com>
2012-07-04 00:13:43 -0400

    o super simple zip plugin to package update sites, so i don't have to use the assembly plugin or more accurately i don't want projects to have to create an assembly descriptor for a task that is the same across all feature deployments

---
+++ src/main/java/io/tesla/tycho/ZipMojo.java
@@ -0,0 +1,110 @@
+package io.tesla.tycho;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+/**
+ * @goal zip
+ * @phase package
+ */
+public class ZipMojo extends AbstractMojo {
+
+  /**
+   * @parameter
+   * @required
+   */
+  private File sourceDirectory;
+
+  /**
+   * @parameter
+   * @required
+   */
+  private File zip;
+
+  public void execute() throws MojoExecutionException {
+    try {
+      zip(sourceDirectory, zip);
+    } catch (IOException e) {
+      throw new MojoExecutionException("Failed to create feature zip.", e);
+    }
+  }
+
+  public static void zip(File directory, File zipfile) throws IOException {
+    URI base = directory.toURI();
+    Deque<File> queue = new LinkedList<File>();
+    queue.push(directory);
+    OutputStream out = new FileOutputStream(zipfile);
+    Closeable res = out;
+    try {
+      ZipOutputStream zout = new ZipOutputStream(out);
+      res = zout;
+      while (!queue.isEmpty()) {
+        directory = queue.pop();
+        for (File kid : directory.listFiles()) {
+          String name = base.relativize(kid.toURI()).getPath();
+          if (kid.isDirectory()) {
+            queue.push(kid);
+            name = name.endsWith("/") ? name : name + "/";
+            zout.putNextEntry(new ZipEntry(name));
+          } else {
+            zout.putNextEntry(new ZipEntry(name));
+            copy(kid, zout);
+            zout.closeEntry();
+          }
+        }
+      }
+    } finally {
+      res.close();
+    }
+  }
+
+  private static void copy(InputStream in, OutputStream out) throws IOException {
+    byte[] buffer = new byte[1024];
+    while (true) {
+      int readCount = in.read(buffer);
+      if (readCount < 0) {
+        break;
+      }
+      out.write(buffer, 0, readCount);
+    }
+  }
+
+  private static void copy(File file, OutputStream out) throws IOException {
+    InputStream in = new FileInputStream(file);
+    try {
+      copy(in, out);
+    } finally {
+      in.close();
+    }
+  }
+}
